69c68a80f9d524922b48c41847cccc06ce8a2928
BrayanVR31 <brayan.vazquez.ramirez.2000@gmail.com>
2026-01-17 19:27:34 -0600

    fix(timeline): fix local zone time

---
+++ src/components/timeline/constants.ts
@@ -0,0 +1,10 @@
+// Date format options depending timezone
+export const formatOptions: Intl.DateTimeFormatOptions = {
+  timeZone: "America/Mexico_City",
+  weekday: "short",
+  day: "2-digit",
+  month: "short",
+  year: "numeric",
+};
+
+export const localZone: Intl.LocalesArgument = "es-MX";
