adb8a30619f6d8bde8d909d44eea3c10db0afe12
BrayanVR31 <brayan.vazquez.ramirez.2000@gmail.com>
2026-01-21 22:22:45 -0600

    feat: add church location map with routing

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -1,11 +1,59 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export default function useLocation() {
-  const [currentPosition, setCurrentPosition] = useState<[lat: number, long: number] | null>(null);
+type Coordinates = [latitude: number, longitude: number];
+type GeoSupport =
+  | {
+      type: "supported";
+      geolocation: Geolocation;
+    }
+  | {
+      type: "error";
+      error: Error;
+    };
+
+/**
+ * Verify fully support for older browser
+ * on Geolocation API.
+ */
+function geoLocationSupport(): GeoSupport {
+  if (!window.navigator.geolocation)
+    return {
+      type: "error",
+      error: new Error("Your browser doesn't support Geolocation services."),
+    };
+  return {
+    type: "supported",
+    geolocation: window.navigator.geolocation,
+  };
+}
+
+const positionOptions: PositionOptions = {
+  enableHighAccuracy: true,
+  timeout: 10_000, // 10s
+  maximumAge: 0,
+};
+
+/**
+ * This hook return the coordinates of browser user
+ * or current position (lat, long)
+ */
+export default function useGeoLocation(): Coordinates | null {
+  const [coords, setCoords] = useState<Coordinates | null>(null);
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
-      setCurrentPosition([latitude, longitude]);
-    }, (e) => e)
+    const geoSupport = geoLocationSupport();
+    // Debugging support errors here
+    if (geoSupport.type === "error") return;
+    const { geolocation } = geoSupport;
+    // Geolocation event handlers (success & error)
+    const success: PositionCallback = (geoPos) => {
+      const {
+        coords: { latitude, longitude },
+      } = geoPos;
+      setCoords([latitude, longitude]);
+    };
+    const error: PositionErrorCallback = () => {};
+    const geoId = geolocation.watchPosition(success, error, positionOptions);
+    return () => geolocation.clearWatch(geoId);
   }, []);
+  return coords;
 }
