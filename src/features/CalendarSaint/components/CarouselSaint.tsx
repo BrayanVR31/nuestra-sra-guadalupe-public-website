eecfc9470505a0d7bba5fe8a3ff5d378d4868c92
BrayanVR31 <brayan.vazquez.ramirez.2000@gmail.com>
2026-02-12 23:40:07 -0600

    fix(saint day): change fetching api on json api

--- src/features/CalendarSaint/components/CarouselSaint.tsx
+++ src/features/CalendarSaint/components/CarouselSaint.tsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
 import EmblaCarousel from "../../../components/common/EmblaCarousel";
 import type { Saint } from "../types/saint.type";
 import CardSaint from "./CardSaint";
 
-interface CarouseSaintProps {
-  saints: Saint[];
-}
-
-export default function CarouseSaint({ saints }: CarouseSaintProps) {
+export default function CarouseSaint() {
+  // TODO: Create a custom hook for handling errors and fetch
+  const [saints, setSaints] = useState<Saint[]>([]);
+  const getSaintDay = async () => {
+    try {
+      const response = await fetch("/api/saintday.json");
+      const data = await response.json();
+      setSaints(data);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    getSaintDay();
+  }, []);
   return (
     <EmblaCarousel
       render={(slideClass) => (
