c7df406485e0f9435f63f477d88035e894fd90f1
BrayanVR31 <brayan.vazquez.ramirez.2000@gmail.com>
2026-02-14 00:45:35 -0600

    fix(ui page): add new design on contact page

---
+++ src/features/Contact/components/ChurchMap.tsx
@@ -0,0 +1,14 @@
+export default function ChurchMap() {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3002.775352892666!2d-98.2473172411866!3d18.977117744008762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfb8ebef131409%3A0xba24d3fc0489d5ee!2sParroquia%20de%20Nuestra%20Se%C3%B1ora%20de%20Guadalupe!5e0!3m2!1ses-419!2smx!4v1771032373036!5m2!1ses-419!2smx"
+      width="100%"
+      height="100%"
+      style={{ border: "0" }}
+      allowFullScreen
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+      title="Mapa De La Parroquia"
+    />
+  );
+}
