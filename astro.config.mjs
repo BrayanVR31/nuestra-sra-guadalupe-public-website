36625b580d11091b9f86f8814cafe9c3d5d3b71a
BrayanVR31 <brayan.vazquez.ramirez.2000@gmail.com>
2026-02-20 13:45:13 -0600

    add sitemap integration for better search web crawlers

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,16 @@ import react from "@astrojs/react";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), icon()],
+  site: "https://www.parroquianuestrasraguadalupe.org.mx",
+  integrations: [react(), icon(), sitemap()],
   adapter: vercel(),
   image: {
     domains: ["res.cloudinary.com"],
